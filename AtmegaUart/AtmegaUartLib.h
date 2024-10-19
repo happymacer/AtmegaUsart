00d6100c3fecb635faea16be9bc7ec0c6687008f
Happymacer <78244043+happymacer@users.noreply.github.com>
2024-10-19 21:39:13 +0800

    Add files via upload

---
+++ AtmegaUart/AtmegaUartLib.h
@@ -0,0 +1,32 @@
+/********************************************************************************
+* This Serial TX definition is copied direct from the							*
+* AVR ATMEGA328P reference manual												*
+*																				*
+* To use it check and adjust the defines below as needed.  Connect pin D1 and	*
+* ground to a USB to RS232 adaptor.	Also set the baud rate.						*
+*																				*
+* start the Usart by:															*
+*	USART_Init(baud rate);															*
+*																				*
+* V1 - 26/9/24 - works									 						*
+*																				*
+*********************************************************************************/
+
+#ifndef AtmegaUartLib_H
+#define AtmegaUartLib_H
+
+#include <avr/io.h>
+
+// defines
+#ifndef F_CPU
+	#define F_CPU 8000000UL
+#endif /*F_CPU*/
+//#define BAUD 500000  //in bits per second - see table page 163 of manual for 328P
+//#define MYUBRR ((F_CPU/16/BAUD)-1)
+
+
+// prototypes
+void USART_Init(unsigned int BAUD);
+void USART_Transmit(unsigned char data);
+
+#endif /*AtmegaUartLib_H*/
---
+++ AtmegaUart/main.c
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * AtmegaUart.c
+ *
+ * Created: 24/08/2024 
+ * Author : Happymacer
+ *  
+ * Version
+ *	1 - works to transmit only
+ *
+ * For now, Im just sending data to the PC.  Receiving can come later - also simplifies and minimises code. 
+ *
+ ****************************************************************************/ 
+
+//includes
+#include <avr/io.h>
+#include <stdint.h>
+#include <string.h>
+#include "AtmegaUartLib.h"
+
+#define F_CPU 8000000UL
+
+//main program
+int main(void)
+{	
+	USART_Init(9600);
+
+	char str[] = "Hello World";
+	for (uint8_t i=0; i<strlen(str); i++)
+	{
+		USART_Transmit(str[i]);
+	}
+	USART_Transmit(13); //ASCII CR character
+	USART_Transmit(10); //ASCII LF character
+
+	for (uint8_t i=0; i<10; i++)
+	{
+		USART_Transmit(i+48); //48 in ascii is 0 -digits range from 48 to 57
+	}
+	USART_Transmit(13); 
+	USART_Transmit(10); 
+	USART_Transmit(7); //ASCII Bell character - windows does produce a tone
+	
+    while (1) 
+    {
+		
+    }
+}
+
+
+
+
+
+
+
+
